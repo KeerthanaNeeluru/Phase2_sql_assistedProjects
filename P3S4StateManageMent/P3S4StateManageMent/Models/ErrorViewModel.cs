0c1d6c9ea3275ad54a2288c0dff9692fda95c93c
keerthana <keerthananeeluru2001@gmail.com>
2023-09-06 23:40:00 +0530

    commit

---
+++ P3S4StateManageMent/P3S4StateManageMent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P3S4StateManageMent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
