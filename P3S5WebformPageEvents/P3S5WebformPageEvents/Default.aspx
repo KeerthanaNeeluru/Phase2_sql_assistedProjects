94cbb9ff1c5be736ccfe18d46dbe77333cb17997
keerthana <keerthananeeluru2001@gmail.com>
2023-09-06 07:53:53 +0530

    commit

---
+++ P3S5WebformPageEvents/P3S5WebformPageEvents/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="P3S5WebformPageEvents._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>PageEvents</h2>
+    <asp:Label ID="lblName" runat="server" CssClass="h4"></asp:Label>
+</asp:Content>
+
---
+++ P3S5WebformPageEvents/P3S5WebformPageEvents/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P3S5WebformPageEvents
+{
+    public partial class _Default : Page
+    {
+        string log = "";
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            log += "page_preinit()<br>";
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            log += "pre_init()<br>";
+        }
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            log += "pre_initComplete()<br>";
+        }
+        protected override void OnPreLoad(EventArgs e)
+        {
+            log += "Onpreload()<br";
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            log += "pre_Load()<br>";
+            lblName.Text=log;
+        }
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            log += "pre_Load()<br>";
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            log += "onprerender()<br>";
+        }
+        protected override void OnSaveStateComplete(EventArgs e)
+        {
+            log+="OnSaveStateComplete()<br>";
+        }
+        protected void Page_UnLoad(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
