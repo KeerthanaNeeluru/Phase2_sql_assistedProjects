e31b6bd9b0345be5c350471d3e332be5178003b8
keerthana <keerthananeeluru2001@gmail.com>
2023-09-07 08:56:09 +0530

    commit

---
+++ P3S4ErrorHandling/P3S4ErrorHandling/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3S4ErrorHandling.Models;
+using System.Diagnostics;
+
+namespace P3S4ErrorHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            int age = Convert.ToInt16(form["age"]);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index2(FormCollection form)
+        {
+            try
+            {
+                int age = Convert.ToInt16(form["age"]);
+            }
+            catch(Exception ex)
+            {
+             
+                throw ex;
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
