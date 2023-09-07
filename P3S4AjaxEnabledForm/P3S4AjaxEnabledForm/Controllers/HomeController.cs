0ca05b7803feb62ab8af2df032e97f35ae2309de
keerthana <keerthananeeluru2001@gmail.com>
2023-09-07 08:34:37 +0530

    commit

---
+++ P3S4AjaxEnabledForm/P3S4AjaxEnabledForm/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3S4AjaxEnabledForm.Models;
+using System.Diagnostics;
+using System.Text;
+
+namespace P3S4AjaxEnabledForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(StudentModel model)
+        {
+            StringBuilder sb = new StringBuilder("Form Data :\n");
+            sb.Append(model.Name + "," + model.Address + "," + model.Email + "," + model.Class);
+            return Content(sb.ToString());
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P3S4AjaxEnabledForm/P3S4AjaxEnabledForm/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace P3S4AjaxEnabledForm.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+    }
+}
