9ba25d465a031c12ed66bb874f6b3f7732370151
keerthana <keerthananeeluru2001@gmail.com>
2023-09-07 07:31:07 +0530

    commit

---
+++ P3S4CustomFilters/P3S4CustomFilters/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P3S4CustomFilters.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ P3S4CustomFilters/P3S4CustomFilters/Controllers/ActionLoggingFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace P3S4CustomFilters.Controllers
+{
+    public class ActionLoggingFilter : ActionFilterAttribute
+    {
+      public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData);
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("OnActionExecuting", filterContext.RouteData);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext) => Log("OnResultExecuting", filterContext.RouteData);
+        private void Log(string methodName,RouteData routeData)
+        {
+            var controllectName = routeData.Values["contoller"];
+            var actionName = routeData.Values["action"];
+            var message = String.Format("{0}-controller:{1} action: {2}", methodName,controllectName,actionName);
+            Debug.WriteLine(message);
+
+        }
+    }
+}
