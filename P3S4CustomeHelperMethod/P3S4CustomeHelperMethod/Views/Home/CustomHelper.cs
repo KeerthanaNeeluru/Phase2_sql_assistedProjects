2a8c6c2f0563c6bffe255f53588eb490c394c290
keerthana <keerthananeeluru2001@gmail.com>
2023-09-07 15:07:54 +0530

    commit

---
+++ P3S4CustomeHelperMethod/P3S4CustomeHelperMethod/Views/Home/CustomHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ServiceStack;
+using System.Web.Mvc;
+using TagBuilder = System.Web.Mvc.TagBuilder;
+
+namespace P3S4CustomeHelperMethod.Views.Home
+{
+    public class CustomHelper
+    {
+        public static IHtmlString File(string id)
+        {
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("id", id);
+            return MvcHtmlString(tb.ToString());
+        }
+    }
+}
